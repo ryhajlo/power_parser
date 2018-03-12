2a61aa30075833e5dffdf8c19b93e9e93e451e1c
Nick Ryhajlo <nick.ryhajlo@gmail.com>
2018-03-11 17:06:46 -0700

    Updated to correctly parse entries in the CSV into a dictionary for each line.

--- power_parser.py
+++ power_parser.py
@@ -2,6 +2,8 @@
 """Take usage data .csv file from SCE, parse it, and analyze it"""
 import sys
 import os
+import re
+import datetime
 
 def print_usage(error_string=""):
     """Print usage in the case of bad inputs"""
@@ -15,16 +17,62 @@ def print_usage(error_string=""):
     print "=========================="
     raise ValueError("Incorrect Parameters")
 
-def main(args):
-    """Main function. Takes a list of csv filenames/paths"""
-    #We need at least one argument, the csv file we are going to process.
+def verify_inputs(args):
+    """Verify the validity of the inputs.
+       We need at least one argument, the csv file we are going to process."""
     if not args:
         print_usage(error_string="Not enough arguments")
+        return False
     for csv_file_path in args:
         if not os.path.exists(csv_file_path):
             print_usage(error_string="Cannot locate file: " + str(csv_file_path))
-        else:
-            print csv_file_path + " exists"
+            return False
+    return True
+
+def convert_date_dict_to_datetime(date_dict):
+    """Take in a dictionary and return datetime"""
+    return datetime.datetime(int(date_dict['year']), int(date_dict['month']), \
+        int(date_dict['day']), int(date_dict['hour']), \
+        int(date_dict['minute']), int(date_dict['second']))
+
+def get_dates_from_line(csv_line):
+    """Take in CSV line. If it is a line that contains two dates, extract and return them"""
+    line_regex = re.compile(r'((?P<year>(?:19|20)\d\d)([- /.])' + \
+        '(?P<month>0[1-9]|1[012])-(?P<day>0[1-9]|[12][0-9]|3[01])' + \
+        '.(?P<hour>[0-1][0-9]|2[0-3]):(?P<minute>[0-5][0-9]):(?P<second>[0-5][0-9]))')
+    dates = [m.groupdict() for m in line_regex.finditer(csv_line)]
+    if len(dates) >= 2:
+        #Make sure we get both a start and end time
+        start_date = convert_date_dict_to_datetime(dates[0])
+        end_date = convert_date_dict_to_datetime(dates[1])
+        return [start_date, end_date]
+    return None
+
+def parse_csv_file(csv_path):
+    """Read in a CSV file and return the data"""
+    # This is only a CSV in that it's values are separted by commas.
+    # There is a lot of non-tabular data in this file.
+    consumption_data = []
+    with open(csv_path) as csv_file_handle:
+        for line in csv_file_handle:
+            #Replace non-printable characters with a space
+            line = re.sub(r'[^\x00-\x7F]+', ' ', line)
+            times = get_dates_from_line(line)
+            if times:
+                split_line = line.split(",")
+                #Second column contains usage data we are interested in
+                consumption_value = float(split_line[1].replace('"', ''))
+                #The end time is implied. Data is in one hour increments.
+                consumption_data.append({'time': times[0], 'value': consumption_value})
+    return consumption_data
+
+def main(args):
+    """Main function. Takes a list of csv filenames/paths"""
+    verify_inputs(args)
+
+    parsed_files = []
+    for csv_file in args:
+        parsed_files.append(parse_csv_file(csv_file))
 
     return True
 
--- unit_tests/power_parser_test.py
+++ unit_tests/power_parser_test.py
@@ -1,26 +1,46 @@
 """Unit tests for power_parser"""
+import datetime
 import unittest
 import power_parser
+
+class TestPowerParserCSVParsing(unittest.TestCase):
+    """Unit tests for parsing csv files"""
+    def test_good_file_parsing(self):
+        """Verify handling of not providing enough parameters"""
+        usage_data = power_parser.parse_csv_file("unit_tests\\usage_data.csv")
+        self.assertEqual(len(usage_data), 1311, "There are 1311 entries in the sample CSV file")
+        #Check first entry
+        self.assertEqual(usage_data[0]['value'], 0.79, \
+            "Incorrect usage data for first entry. Expected 0.79 kWh, got " \
+            + str(usage_data[0]['value']))
+        self.assertEqual(usage_data[0]['time'], datetime.datetime(2018, 1, 1, 0, 0), \
+            "Incorrect timestamp for first entry.")
+        #Check last entry
+        self.assertEqual(usage_data[-1]['value'], 0.24, \
+            "Incorrect usage data for last entry. Expected 0.24 kWh, got " \
+            + str(usage_data[-1]['value']))
+        self.assertEqual(usage_data[-1]['time'], datetime.datetime(2018, 2, 24, 23, 0), \
+            "Incorrect timestamp for last entry")
 
 class TestPowerParserParameters(unittest.TestCase):
     """Unit tests for handling parameters from the command line"""
     def test_not_enough_parameters(self):
         """Verify handling of not providing enough parameters"""
         with self.assertRaises(ValueError):
-            power_parser.main([])
+            power_parser.verify_inputs([])
 
     def test_file_doesnt_exist(self):
         """Verify handling of providing a filename that doesn't exist"""
         with self.assertRaises(ValueError):
-            power_parser.main(["doesn_exist.csv"])
+            power_parser.verify_inputs(["doesn_exist.csv"])
 
     def test_handle_good_file(self):
         """Verify handling of providing a filename that doesn't exist"""
-        self.assertTrue(power_parser.main(["unit_tests\\usage_data.csv"]))
+        self.assertTrue(power_parser.verify_inputs(["unit_tests\\usage_data.csv"]))
 
     def test_handle_good_files(self):
         """Verify handling of providing a filename that doesn't exist"""
-        self.assertTrue(power_parser.main([\
+        self.assertTrue(power_parser.verify_inputs([\
             "unit_tests\\usage_data.csv", "unit_tests\\usage_data_old_apartment.csv"]))
 
 if __name__ == '__main__':
